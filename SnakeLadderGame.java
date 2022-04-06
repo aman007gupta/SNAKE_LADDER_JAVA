85c32d7007c4e949800bc854398018365aef321d
aman007gupta <aman9453122969@gmail.com>
2022-04-06 15:13:13 +0530

    Player rolls a die to get no between 1 to 6

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -3,5 +3,7 @@ public class SnakeLadderGame {
 		System.out.println("Welcome to Snake Ladder Game");
 		int startingPosition = 0;
 		System.out.println("Game start at position: " + startingPosition);
+		int noDie = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println("Number on Die: " + noDie);
 	}
 }
