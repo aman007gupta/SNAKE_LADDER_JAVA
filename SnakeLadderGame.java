5a765f4f8b404d34062c6f7872411baaf231ef7b
aman007gupta <aman9453122969@gmail.com>
2022-04-06 15:33:23 +0530

    Player check for options: no play snake and ladder

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -1,9 +1,25 @@
 public class SnakeLadderGame { 
+	public static final int IS_LADDER = 1;
+	public static final int IS_SNAKE = 2;
 	public static void main (String args[]) {
 		System.out.println("Welcome to Snake Ladder Game");
 		int startingPosition = 0;
+		int currentPosition = 0;
 		System.out.println("Game start at position: " + startingPosition);
 		int noDie = (int) Math.floor(Math.random() * 10) % 6 + 1;
 		System.out.println("Number on Die: " + noDie);
+		int options = (int) Math.floor(Math.random() * 10) % 3;
+		switch (options) {
+			case IS_LADDER:
+				currentPosition = currentPosition + noDie;
+				break;
+			case IS_SNAKE:
+				currentPosition = currentPosition - noDie;
+				break;
+			default:
+				currentPosition = currentPosition;
+		}
+		System.out.println("Current position: " + currentPosition);
+
 	}
 }
