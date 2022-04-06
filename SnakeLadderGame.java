4cbad779d89a53bbbd855c59fb5d991ee187dc5a
aman007gupta <aman9453122969@gmail.com>
2022-04-06 14:59:29 +0530

    Start game at position 0

---
+++ SnakeLadderGame.java
@@ -0,0 +1,7 @@
+public class SnakeLadderGame { 
+	public static void main (String args[]) {
+		System.out.println("Welcome to Snake Ladder Game");
+		int startingPosition = 0;
+		System.out.println("Game start at position: " + startingPosition);
+	}
+}
