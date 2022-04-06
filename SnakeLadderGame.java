cb8011a107c4987afe0054afac577fcf14933feb
aman007gupta <aman9453122969@gmail.com>
2022-04-06 16:23:08 +0530

    position every die and no of time dice rollss

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -5,32 +5,35 @@ public class SnakeLadderGame {
 		System.out.println("Welcome to Snake Ladder Game");
 		int startingPosition = 0;
 		int currentPosition = 0;
+		int count = 0;
 		System.out.println("Game start at position: " + startingPosition);
-		while (currentPosition <= 100) {
-		int noDie = (int) Math.floor(Math.random() * 10) % 6 + 1;
-		System.out.println("Number on Die: " + noDie);
-		int options = (int) Math.floor(Math.random() * 10) % 3;
-		switch (options) {
-			case IS_LADDER:
-				currentPosition = currentPosition + noDie;
-				break;
-			case IS_SNAKE:
+		while (currentPosition < 100) {
+			count++;
+			int noDie = (int) Math.floor(Math.random() * 10) % 6 + 1;
+			System.out.println("Number on Die: " + noDie);
+			int options = (int) Math.floor(Math.random() * 10) % 3;
+			switch (options) {
+				case IS_LADDER:
+					currentPosition = currentPosition + noDie;
+					break;
+				case IS_SNAKE:
+					currentPosition = currentPosition - noDie;
+					if (currentPosition < 1) {
+						currentPosition = 0;
+					}
+					break;
+				default:
+					currentPosition = currentPosition;
+			}
+			System.out.println("Current Position: " + currentPosition);
+			if (currentPosition > 100) {
 				currentPosition = currentPosition - noDie;
-				if (currentPosition < 1) {
-					currentPosition = 0;
-				}
-				break;
-			default:
-				currentPosition = currentPosition;
+				System.out.println("Current Position: " + currentPosition);
+			} else if (currentPosition == 100) {
+				System.out.println("Player reached: " + currentPosition +" Player Won!");
+			}
 		}
-		if (currentPosition > 100) {
-			currentPosition = currentPosition - noDie;
-		} else if (currentPosition == 100) {
-			System.out.println("Player reached: " + currentPosition +" Player Won!");
-			break;
-		}
-		System.out.println("Current position: " + currentPosition);
-		}
+		System.out.println("Dice roll: " + count);
 
 	}
 }
